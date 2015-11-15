794e69ec7d490e9b7e13f6bff7d6726c93c24ee8
Daniel Brim <brimizer@gmail.com>
2015-11-15 15:03:04 -0800

    test

--- js/notifications.js
+++ js/notifications.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function() {
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/worker.js')
+    navigator.serviceWorker.register('../js/worker.js')
     .then(initialiseState);
   } else {
     console.warn('Service workers aren\'t supported in this browser.');
