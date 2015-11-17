7de747ed9765fced53ef0097f902b29f4977d130
Daniel Brim <brimizer@gmail.com>
2015-11-17 01:45:54 -0800

    event data

--- worker.js
+++ worker.js
@@ -28,7 +28,6 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  var url = event.notification.data.url;
   event.notification.close();
   event.waitUntil(clients.openWindow("http://katiazee.github.io/Vice-Virtue/src/list.html"));
 });
