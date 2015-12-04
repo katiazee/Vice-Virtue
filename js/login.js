cbd76a847b02b8388b29cc831827ce1261f9bde9
Daniel Brim <brimizer@gmail.com>
2015-12-04 12:23:04 -0800

    added full js back in

---
+++ js/login.js
@@ -0,0 +1,43 @@
+Parse.initialize("Ih70t530LwJAnRJungwuPtE2nE3eakzmwVuZHb6O", "sE6Y6iMcoTTa9Z3pBCaAtwnD9F1L9SlWHBKlDQ7h");
+
+rg4js('apiKey', '5HU2mFdkS/e5Tur8l31gcA==');
+rg4js('attach', true);
+rg4js('enablePulse', true);
+
+rg4js('setUser', {
+  identifier: 'users_email_address_or_unique_id@domain.com',
+  isAnonymous: false,
+  email: 'users_email_address@domain.com',
+  firstName: 'Firstname',
+  fullName: 'Firstname Lastname'
+});
+
+function onClickSignUp() {
+	var signUpText = document.getElementById("signInMessage");
+	signUpText.style.display = "block";
+}
+
+function signIn() {
+	var userEmail = document.getElementById('usermail').value;
+	var password = document.getElementById('password').value;
+
+	// Parse.User.logOut();
+	document.getElementById('usermail').className = "textInput";
+	document.getElementById('password').className = "textInput";
+	document.getElementById('invalidLoginError').style.display = 'none';
+
+	Parse.User.logIn(userEmail, password, {
+		success: function(user) {
+			// Do stuff after successful login.
+			// alert(userEmail + ' was successfully logged in!');
+			location = "welcome.html";
+		},
+		error: function(user, error) {
+			// The login failed. Check error to see why.
+			// alert("Error: " + error.code + " " + error.message);
+			document.getElementById('usermail').className = document.getElementById('usermail').className + " inputError";
+			document.getElementById('password').className = document.getElementById('password').className + " inputError";
+			document.getElementById('invalidLoginError').style.display = 'block';
+		}
+	});
+}
---
+++ js/signup.js
@@ -0,0 +1,118 @@
+Parse.initialize("Ih70t530LwJAnRJungwuPtE2nE3eakzmwVuZHb6O", "sE6Y6iMcoTTa9Z3pBCaAtwnD9F1L9SlWHBKlDQ7h");
+
+function onSignUpPress() {
+	var doesEmailValidate = validateEmail();
+	if (validatePassword() && doesEmailValidate) {
+		registerAccount();
+	}
+}
+
+function verifyPasswords() {
+	var password = document.getElementById("validPass").value;
+	var confirmPassword = document.getElementById("validConfirmPass").value;
+
+	if (password == "" || confirmPassword == "") {
+		return false;
+	}
+	else if (password != confirmPassword) {
+		// Display form or what not
+		document.getElementById("validConfirmPass").style.border = '1px solid';
+		document.getElementById("validConfirmPass").style.borderColor = '#b94a48';
+		document.getElementById('passwordsMismatch').style.display = 'block';
+	}
+	else {
+		document.getElementById('passwordsMismatch').style.display = 'none';
+		document.getElementById("validConfirmPass").style.border = '2px inset';
+		return true;
+	}
+}
+
+function validateEmail() {
+	var emailAddress = document.getElementById("validEmail");
+
+	document.getElementById('emailTaken').style.display = 'none';
+	emailAddress.style.border = '2px inset';
+
+	// Validation for email addresss
+	if(emailAddress.value == ""){
+		emailAddress.style.border = '1px solid';
+		emailAddress.style.borderColor = '#b94a48';
+		document.getElementById('noEmail').style.display = 'block';
+		return false;
+	}
+	else{
+		document.getElementById('noEmail').style.display = 'none';
+		emailAddress.style.border = '2px inset';
+		return true;
+	}
+}
+
+function validatePassword(){
+	var createPass = document.getElementById("validPass");
+	var confirmPass = document.getElementById("validConfirmPass");
+	var createPassValid = false;
+	var confirmPassValid = false;
+	var doesValidate = true;
+
+	document.getElementById('passwordsMismatch').style.display = 'none';
+	document.getElementById("validConfirmPass").style.border = '2px inset';
+
+	//Validation for creating a password
+	if(createPass.value == ""){
+		createPass.style.border = '1px solid';
+		createPass.style.borderColor = '#b94a48';
+		document.getElementById('noPass').style.display = 'block';
+		doesValidate = false;
+	}
+	else{
+		createPass.style.border = '2px inset';
+		document.getElementById('noPass').style.display = 'none';
+		// createPassValid = true;
+	}
+
+	//Validation for confirming password
+	if(confirmPass.value == ""){
+		confirmPass.style.border = '1px solid';
+		confirmPass.style.borderColor = '#b94a48';
+		document.getElementById('noConfirmPass').style.display = 'block';
+		doesValidate = false;
+	}
+	else{
+		document.getElementById('noConfirmPass').style.display = 'none';
+		confirmPass.style.border = '2px inset';
+		// confirmPassValid = true;
+	}
+
+	// If passwords both have values; check if they are the same
+	if (doesValidate) {
+		doesValidate = verifyPasswords();
+	}
+
+	return doesValidate;
+}
+
+function registerAccount() {
+	var user = new Parse.User();
+	user.set("username", document.getElementById("validEmail").value);
+	user.set("password", document.getElementById("validPass").value);
+	user.set("email", document.getElementById("validEmail").value);
+
+	user.signUp(null, {
+		success: function(user) {
+			location = "welcome.html";
+		},
+		error: function(user, error) {
+			// User already taken error
+			if (error.code == 202) {
+				document.getElementById("validEmail").style.border = '1px solid';
+				document.getElementById("validEmail").style.borderColor = '#b94a48';
+				document.getElementById('emailTaken').style.display = 'block';
+			}
+			else {
+				// Other error
+				alert("Error: " + error.code + " " + error.message);
+			}
+		}
+	});
+}
+
